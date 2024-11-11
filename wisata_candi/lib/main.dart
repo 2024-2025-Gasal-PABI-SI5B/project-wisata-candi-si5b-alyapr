8e34c584a13d3fb4847fbb5532b9f86f6644d61b
alyapr <putririzaldyalya@gmail.com>
2024-11-11 11:21:31 +0700

    next ke part 3

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,15 +14,11 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Wisata Candi",
-      theme : ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true 
-      ),
-      home: DetailScreen(
-        candi: candiList[0],
-        ),
-      );
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true),
+      // home: DetailScreen(candi: candiList[0],),
+      home: ProfileScreen(),
+    );
   }
 }
-
-
